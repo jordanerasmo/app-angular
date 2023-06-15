53c5a0b9e4461f0d794fa9052941a722ec203f39
Erasmo Jordan <erasmo7jordan@gmail.com>
2023-06-15 14:08:42 -0300

    Add vehicles actions

---
+++ src/app/state/vehicles.actions.ts
@@ -0,0 +1,19 @@
+import { createAction, props } from "@ngrx/store"
+import { Vehicle } from "../Interfaces/Vehicle"
+
+export const activePreload = createAction('[Vehicles Page] Preload Vehicles List')
+
+export const getVehicles = createAction('[Vehicles Page] Get Vehicles List', 
+props<{ vehicles: Vehicle[]}>())
+
+export const getVehicle = createAction('[Vehicles Page] Get Vehicle', 
+props<{ vehicle: Vehicle}>())
+
+export const addVehicle = createAction('[Vehicles Page] Add Vehicle', 
+props<{ vehicle: Vehicle}>())
+
+export const updateVehicle = createAction('[Vehicles Page] Update Vehicle', 
+props<{ vehicle: Vehicle}>())
+
+export const deleteVehicle = createAction('[Vehicles Page] Delete Vehicle', 
+props<{ vehicle: Vehicle}>())
