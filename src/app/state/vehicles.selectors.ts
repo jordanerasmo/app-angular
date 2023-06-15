1e45e21b51fbf5e4fe48cf58d069a03d1de76e9b
Erasmo Jordan <erasmo7jordan@gmail.com>
2023-06-15 14:09:20 -0300

    Add vehicles selectors

---
+++ src/app/state/vehicles.selectors.ts
@@ -0,0 +1,16 @@
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { VehiclesState } from "./vehicles.reducer";
+
+
+const vehiclesState = createFeatureSelector<VehiclesState>('vehiclesState')
+
+//Selector for Vehicles List
+export const vehicles = createSelector(
+    vehiclesState,
+    (vehiclesState) => vehiclesState.vehicles
+)
+
+export const preload = createSelector(
+    vehiclesState,
+    (vehiclesState) => vehiclesState.preload
+)
