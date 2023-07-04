a61fcc4bb18b0d0f878554b0309c439e8d311de3
Erasmo Jordan <erasmo7jordan@gmail.com>
2023-07-04 00:27:05 -0300

    delete refresh data with subject

--- src/app/services/vehiculo.service.ts
+++ src/app/services/vehiculo.service.ts
@@ -3,25 +3,15 @@ import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Observable, Subject, tap } from 'rxjs';
 import { Vehicle as Vehicle } from '../vehicles/models/vehicle.model';
 
-const HttpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-  }),
-};
-
 @Injectable({
   providedIn: 'root'
 })
+
 export class VehiculoService {
 
   constructor(private http: HttpClient) { }
 
   private apiUrl = 'http://localhost:5000/vehiculos'
-  private _refreshdata = new Subject<void>();
-  
-  get RefreshData(){
-    return this._refreshdata;
-  }
   
   @Output() mediador: EventEmitter<any> = new EventEmitter();
 
@@ -36,27 +26,15 @@ export class VehiculoService {
 
   deleteVehiculo(vehiculo: Vehicle){
     const url = `${this.apiUrl}/${vehiculo.id}`;
-    return this.http.delete<Vehicle>(url).pipe(
-      tap(() => {
-        this.RefreshData.next();
-      })
-    );
+    return this.http.delete<Vehicle>(url)
   }
 
   addVehiculo(vehiculo: Vehicle): Observable<Vehicle> {
-    return this.http.post<Vehicle>(this.apiUrl, vehiculo).pipe(
-      tap(() => {
-        this.RefreshData.next();
-      })
-    );
+    return this.http.post<Vehicle>(this.apiUrl, vehiculo)
   }
 
   updateVehiculo(editId:Number, vehiculo: Vehicle): Observable<Vehicle> {
     const url = `${this.apiUrl}/${editId}`;
-    return this.http.put<Vehicle>(url, vehiculo).pipe(
-      tap(() => {
-        this.RefreshData.next();
-      })
-    );
+    return this.http.put<Vehicle>(url, vehiculo)
   }
 }
